0c1cec51a36e939ba25c17e42248d8c52325cd10
Pabl0Maciel <pablocaballero07@gmail.com>
2024-05-01 17:08:02 -0300

    Update distancia.py

--- Python/semana_3/distancia.py
+++ Python/semana_3/distancia.py
@@ -4,7 +4,7 @@ y1=int(input("Digite a ordenada do primeiro ponto: "))
 x2=int(input("Digite a abscissa do segundo ponto: "))
 y2=int(input("Digite a ordenada do segundo ponto: "))
 
-dist=math.sqrt(((x1-x2)*2)+((y1-y2)*2))
+dist=math.sqrt(((x1-x2)**2)+((y1-y2)**2))
 
 if dist>=10:
     print("longe")
