1722c752820e0284d633ba4910e595cb2dc80001
Pabl0Maciel <pablocaballero07@gmail.com>
2024-05-02 19:38:34 -0300

    Update fatorial.py

--- Python/semana_4/fatorial.py
+++ Python/semana_4/fatorial.py
@@ -1,8 +1,13 @@
 n=int(input("Digite o valor de n: "))
 i=n-1
 fatorial=n
-while i>0 and n!=0:
-    fatorial=fatorial*i
+zero=False
+while (i>0 or n==0) and (not zero):
+    if n==0:
+        fatorial=1 
+        zero=True
+    else:
+        fatorial=fatorial*i
     i=i-1
     
 print(fatorial)
