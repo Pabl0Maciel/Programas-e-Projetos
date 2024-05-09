1af3a2f2a52d6af7e8607a19883329e43b77dfc6
Pabl0Maciel <pablocaballero07@gmail.com>
2024-05-09 16:12:55 -0300

    organizacao

---
+++ C/Aula 09_05_24/Ordenacao/bubblesort.c
@@ -0,0 +1,26 @@
+void bubbleSort2(int *V  , int N){//otimizado
+    int i, continua, aux, fim = N;
+    do{
+        continua = 0;
+        for(i = 0; i < fim-1; i++){
+            if (V[i] > V[i+1]){
+                aux = V[i];
+                V[i] = V[i+1];
+                V[i+1] = aux;
+                continua = i;
+            }
+        }
+        fim--;
+    }while(continua != 0);
+}
+
+int main(){
+    int i;
+    int vet[10] = {23,4,67,-8,54,90,21,14,-5,1};
+    int N = 10;
+
+    printf("Sem ordenar:\n");
+    bubbleSort2(vet,N);
+    for(i=0; i<N; i++){
+        printf("%d\n",vet[i]);
+    }
