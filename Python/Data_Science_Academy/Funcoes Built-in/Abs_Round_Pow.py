1a1e7afb6f7fa09fedc38e6743ee2bdc336cb722
Pabl0Maciel <pablocaballero07@gmail.com>
2024-07-28 15:00:18 -0300

    Renomeamento de pastas e arquivos

--- Python/Data_Science_Academy/Funcoes Built-in/Abs_Round_Pow.py
+++
@@ -1,5 +0,0 @@
-print(abs(-8)) #retorna valor absoluto
-
-print(round(3.14151922, 2)) #retorna o valor com o numero de casas decimais escolhidas (no caso 2)
-
-print(pow(4,2)) #potencia (pode usar **)
