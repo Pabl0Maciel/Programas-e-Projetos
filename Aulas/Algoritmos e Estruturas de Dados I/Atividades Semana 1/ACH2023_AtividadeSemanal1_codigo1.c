fdc6bd326a4dd7ae123ebd67730d097dc06ed441
Pabl0Maciel <pablocaballero07@gmail.com>
2024-08-07 17:26:49 -0300

    exercicio semana 1

--- Aulas/Algoritmos e Estruturas de Dados I/Atividades Semana 1/ACH2023_AtividadeSemanal1_codigo1.c
+++ Aulas/Algoritmos e Estruturas de Dados I/Atividades Semana 1/ACH2023_AtividadeSemanal1_codigo1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int nusp = 12345678;
+    int nusp = 14778630;
     printf("Imprimindo inteiro: %i\n", nusp);
     printf("Imprimindo numero: %d\n", nusp);
     printf("Imprimindo float (com cast): %f\n", (float)nusp);
