455db59b0201f62334a32e838859bb94fe908cff
Pabl0Maciel <pablocaballero07@gmail.com>
2024-08-26 21:37:06 -0300

    EP 1 Final

--- Aulas/Introducao a Analise de Algoritmos/EP 1/ep1_14778630.c
+++ Aulas/Introducao a Analise de Algoritmos/EP 1/ep1_14778630.c
@@ -46,7 +46,7 @@ MinMax Acha_MinMax(int **matriz, int lin_inicio, int lin_fim, int col_inicio, in
     Mat3 = Acha_MinMax(matriz,lin_meio+1, lin_fim, col_inicio, col_meio);
     Mat4 = Acha_MinMax(matriz,lin_meio+1, lin_fim, col_meio+1, col_fim);
 
-    // compara minimo entre as quatro matrizes e acha o menor deles
+// compara minimo entre as quatro matrizes e acha o menor deles
     Resultado.Min = Mat1.Min;
     Resultado.LinMin = Mat1.LinMin;
     Resultado.ColMin = Mat1.ColMin;
@@ -56,18 +56,59 @@ MinMax Acha_MinMax(int **matriz, int lin_inicio, int lin_fim, int col_inicio, in
         Resultado.LinMin = Mat2.LinMin;
         Resultado.ColMin = Mat2.ColMin;
     }
+
+    // caso os minimos sejam iguais, verifica quem tem a menor linha e, em caso de empate, ve quem tem a menor coluna
+    if (Mat2.Min == Resultado.Min){
+        if (Mat2.LinMin < Resultado.LinMin){
+            Resultado.LinMin = Mat2.LinMin;
+            Resultado.ColMin = Mat2.ColMin;
+        }
+        if (Mat2.LinMin == Resultado.LinMin){
+            if (Mat2.ColMin < Resultado.ColMin){
+                Resultado.ColMin = Mat2.ColMin;
+            }
+        }
+    }
+    
     if (Mat3.Min < Resultado.Min){
         Resultado.Min = Mat3.Min;
         Resultado.LinMin = Mat3.LinMin;
         Resultado.ColMin = Mat3.ColMin;
     }
+
+    // caso os minimos sejam iguais, verifica quem tem a menor linha e, em caso de empate, ve quem tem a menor coluna
+    if (Mat3.Min == Resultado.Min){
+        if (Mat3.LinMin < Resultado.LinMin){
+            Resultado.LinMin = Mat3.LinMin;
+            Resultado.ColMin = Mat3.ColMin;
+        }
+        if (Mat3.LinMin == Resultado.LinMin){
+            if (Mat3.ColMin < Resultado.ColMin){
+                Resultado.ColMin = Mat3.ColMin;
+            }
+        }
+    }
+
     if (Mat4.Min < Resultado.Min){
         Resultado.Min = Mat4.Min;
         Resultado.LinMin = Mat4.LinMin;
         Resultado.ColMin = Mat4.ColMin;
     }
+    
+    // caso os minimos sejam iguais, verifica quem tem a menor linha e, em caso de empate, ve quem tem a menor coluna
+    if (Mat4.Min == Resultado.Min){
+        if (Mat4.LinMin < Resultado.LinMin){
+            Resultado.LinMin = Mat4.LinMin;
+            Resultado.ColMin = Mat4.ColMin;
+        }
+        if (Mat4.LinMin == Resultado.LinMin){
+            if (Mat4.ColMin < Resultado.ColMin){
+                Resultado.ColMin = Mat4.ColMin;
+            }
+        }
+    }
 
-    // compara o maximo entre as quatro matrizes e acha o menor deles
+// compara o maximo entre as quatro matrizes e acha o menor deles
     Resultado.Max = Mat1.Max;
     Resultado.LinMax = Mat1.LinMax;
     Resultado.ColMax = Mat1.ColMax;
@@ -77,15 +118,57 @@ MinMax Acha_MinMax(int **matriz, int lin_inicio, int lin_fim, int col_inicio, in
         Resultado.LinMax = Mat2.LinMax;
         Resultado.ColMax = Mat2.ColMax;
     }
+
+    // caso os maximos sejam iguais, verifica quem tem a menor linha e, em caso de empate, ve quem tem a menor coluna
+    if (Mat2.Max == Resultado.Max){
+        if (Mat2.LinMax < Resultado.LinMax){
+            Resultado.LinMax = Mat2.LinMax;
+            Resultado.ColMax = Mat2.ColMax;
+        }
+        if (Mat2.LinMax == Resultado.LinMax){
+            if (Mat2.ColMax < Resultado.ColMax){
+                Resultado.ColMax = Mat2.ColMax;
+            }
+        }
+    }
+
     if (Mat3.Max > Resultado.Max){
         Resultado.Max = Mat3.Max;
         Resultado.LinMax = Mat3.LinMax;
         Resultado.ColMax = Mat3.ColMax;
     }
+
+
+    // caso os maximos sejam iguais, verifica quem tem a menor linha e, em caso de empate, ve quem tem a menor coluna
+    if (Mat3.Max == Resultado.Max){
+        if (Mat3.LinMax < Resultado.LinMax){
+            Resultado.LinMax = Mat3.LinMax;
+            Resultado.ColMax = Mat3.ColMax;
+        }
+        if (Mat3.LinMax == Resultado.LinMax){
+            if (Mat3.ColMax < Resultado.ColMax){
+                Resultado.ColMax = Mat3.ColMax;
+            }
+        }
+    }
+
     if (Mat4.Max > Resultado.Max){
         Resultado.Max = Mat4.Max;
         Resultado.LinMax = Mat4.LinMax;
         Resultado.ColMax = Mat4.ColMax;
+    }
+    
+    // caso os maximos sejam iguais, verifica quem tem a menor linha e, em caso de empate, ve quem tem a menor coluna
+    if (Mat4.Max == Resultado.Max){
+        if (Mat4.LinMax < Resultado.LinMax){
+            Resultado.LinMax = Mat4.LinMax;
+            Resultado.ColMax = Mat4.ColMax;
+        }
+        if (Mat4.LinMax == Resultado.LinMax){
+            if (Mat4.ColMax < Resultado.ColMax){
+                Resultado.ColMax = Mat4.ColMax;
+            }
+        }
     }
 
     return Resultado;
@@ -96,7 +179,7 @@ int main(int argc, char *argv[]){
     
     // atribui o valor de N lido
     int N = atoi(argv[1]);
-    printf("Valor de N: %d\n", N);
+    //printf("Valor de N: %d\n", N);
 
     // abre os arquivos de entrada e saida
     FILE *minha_entrada = fopen(argv[2], "r");
@@ -117,7 +200,7 @@ int main(int argc, char *argv[]){
     }
     
     // imprime a matriz
-    imprimeMatriz(matriz, N);
+    //imprimeMatriz(matriz, N);
 
     // resultado
     MinMax Resultado = Acha_MinMax(matriz, 0, N-1, 0, N-1);
