28234b91959845d8c4044fcec97ed7e67e256408
Pabl0Maciel <pablocaballero07@gmail.com>
2024-09-21 11:29:41 -0300

    Problema da Mochila FInalizado

--- Aulas/Introducao a Analise de Algoritmos/Problema da Mochila/problema_mochila.c
+++ Aulas/Introducao a Analise de Algoritmos/Problema da Mochila/problema_mochila.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Este codigo resolve o problema da mochila binaria, em que eh dada uma mochila de tamanho K e se quer saber quais itens de tamanhos guardados no array 'itens' de tamanho n conseguem satisfazer a capacidade da mochila completamente. Caso nao existe nenhuma combinacao, o codigo retorna a combinacao mais proxima do sucesso. */
+
+
+// Estrutura da mochila, onde cada item pode existir e/ou pertencer a mochila
 typedef struct{
     int existe;
     int pertence;
 }MOCHILA;
 
+
+// Imprime a matriz de combinacaoes da mochila
 void imprime_MatrizMochila(MOCHILA **mochila, int n, int K){
     for(int i = 0; i < n; i++){
         printf("I_%d: ", i);
-        for (int j = 0; j < K; j++){
+        for (int j = 0; j <= K; j++){
             printf("| %d / %d | ", mochila[i][j].existe, mochila[i][j].pertence);
         }
         printf("\n");
@@ -17,23 +23,26 @@ void imprime_MatrizMochila(MOCHILA **mochila, int n, int K){
     printf("\n");
 }
 
+// Aloca memoria para a matriz
 void inicializa_MatrizMochila(MOCHILA ***mochila, int n, int K){
     *mochila = (MOCHILA **)malloc(n * sizeof(MOCHILA*));
     for (int i = 0; i < n; i++) {
-        (*mochila)[i] = (MOCHILA *)malloc(K * sizeof(MOCHILA));
+        (*mochila)[i] = (MOCHILA *)malloc((K+1) * sizeof(MOCHILA));
     }
 }
 
+
+// Cria uma matriz com todas as possibilidades de preencher mochila
 void MatrizMochila(MOCHILA **mochila, int *itens, int K, int n){
     mochila[0][0].existe = 0;
 
-    for (int k = 1; k < K; k++){
+    for (int k = 1; k <= K; k++){
         mochila[0][k].existe = -1;
     }
 
     for (int i = 1; i < n; i++){
 
-        for (int k = 0; k < K; k++){
+        for (int k = 0; k <= K; k++){
             mochila[i][k].existe = -1;
 
             if (mochila[i-1][k].existe == 0){
@@ -53,9 +62,9 @@ void MatrizMochila(MOCHILA **mochila, int *itens, int K, int n){
     }
 }
 
+// Encontra os itens que satisfazem o problema
 void resolve_ProblemaMochila(MOCHILA **mochila, int *resp, int n, int K, int *itens){
     int a = 0;
-    K--;
 
     while (K > 0){
         int achou = -1;
@@ -72,6 +81,7 @@ void resolve_ProblemaMochila(MOCHILA **mochila, int *resp, int n, int K, int *it
     }
 }
 
+// Main
 int main(){
 
     MOCHILA **mochila;
