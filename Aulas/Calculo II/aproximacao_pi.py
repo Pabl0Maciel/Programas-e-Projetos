2f4c24cf0879ca04be9cd165f1f60016148691eb
Pabl0Maciel <pablocaballero07@gmail.com>
2024-09-22 12:42:39 -0300

    Calculo correto

--- Aulas/Calculo II/aproximacao_pi.py
+++ Aulas/Calculo II/aproximacao_pi.py
@@ -1,14 +1,56 @@
-from mpmath import mp
+from decimal import Decimal, getcontext
 
-# calcula as 2100 casas decimais
-mp.dps = 2100
+# Ajusta a precisao para 2110 casas decimais
+getcontext().prec = 100 
 
-# formula de K . Takano
-pi_takano = 4 * (12 * mp . atan (1 / 49) + 32 * mp . atan (1 / 57) - 5 * mp . atan (1 / 239) + 12 * mp . atan (1 / 110443) )
+# Calculo de arctan usando a Serie de Taylor
+def arctan(x, terms):
+    result = Decimal(0)
+    for n in range(terms):
+        term = ((-1) ** n) * (x ** (2 * n + 1)) / Decimal(2 * n + 1)
+        result += term
+    return result
 
-# formula de F . C . M . Stormer
-pi_stormer = 4 * (44 * mp . atan (1 / 57) + 7 * mp . atan (1 / 239) - 12 * mp . atan (1 / 682) + 24 * mp . atan (1 / 12943) )
+# Formula de Machin
+def pi_machin(terms):
+    term1 = 4 * arctan(Decimal(1) / 5, terms)
+    term2 = arctan(Decimal(1) / 239, terms)
+    pi_over_4 = term1 - term2
+    return 4 * pi_over_4
 
-print(" Valor de pi usando K . Takano : \n", pi_takano )
-print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-print(" Valor de pi usando F . C . M . Stormer : ", pi_stormer)
+# Formula de K. Takano
+def pi_k_takano(terms):
+    term1 = Decimal(12) * arctan(Decimal(1) / 49, terms)
+    term2 = Decimal(32) * arctan(Decimal(1) / 57, terms)
+    term3 = Decimal(-5) * arctan(Decimal(1) / 239, terms)
+    term4 = Decimal(12) * arctan(Decimal(1) / 110443, terms)
+    pi_over_4 = term1 + term2 + term3 + term4
+    return 4 * pi_over_4
+
+# Formula de Stormer
+def pi_f_stormer(terms):
+    term1 = Decimal(44) * arctan(Decimal(1) / 57, terms)
+    term2 = Decimal(7) * arctan(Decimal(1) / 239, terms)
+    term3 = Decimal(-12) * arctan(Decimal(1) / 682, terms)
+    term4 = Decimal(24) * arctan(Decimal(1) / 12943, terms)
+    pi_over_4 = term1 + term2 + term3 + term4
+    return 4 * pi_over_4
+
+# Cálculo de pi 
+terms = 1000  # numero de termos usados na Serie de Taylor (quanto mais, maior a precisao)
+
+pi_Machin = pi_machin(terms)
+
+#pi_takano = pi_k_takano(terms)
+#pi_stormer = pi_f_stormer(terms)
+
+# Mostra o Resultado
+print("Pi calculado com a fórmula de Machin:")
+print(str(pi_Machin))
+
+
+#print("Pi calculado com a fórmula de K. Takano:")
+#print(str(pi_takano))
+
+#print("\nPi calculado com a fórmula de F.C.M. Størmer:")
+#print(str(pi_stormer))
