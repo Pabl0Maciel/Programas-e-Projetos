7d3a9aa34997474163d79a13b27cdf565ca9c501
Pabl0Maciel <pablocaballero07@gmail.com>
2024-09-23 18:41:05 -0300

    mudei para descrescente

--- C/Ordenacao/qsort.c
+++ C/Ordenacao/qsort.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int comparaCrescente(const void *a, const void *b){
+int compara_decrescente(const void *a, const void *b){
     if (*(int*)a==*(int*)b){
         return 0;//iguais
     }
     else{
-        if (*(int*)a<*(int*)b){
+        if (*(int*)a > *(int*)b){
             return -1;//vem antes
         }
         else{
@@ -17,7 +17,7 @@ int comparaCrescente(const void *a, const void *b){
 
 int main(){
     int i, vet[10]={23,4,67,-8,54,90,21,14,-5,1};
-    qsort(vet,10,sizeof(int),comparaCrescente);
+    qsort(vet,10,sizeof(int),compara_decrescente);
     for(i=0;i<10;i++){
         printf("Number=%d\n",vet[i]);
     }
