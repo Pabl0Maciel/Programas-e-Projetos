ee08ce5461671fa81a0a2dc5b209aaa3602efcc7
Pabl0Maciel <pablocaballero07@gmail.com>
2025-03-31 11:38:42 -0300

    lista exercicios

--- Aulas/Semestre III/AED II/Grafos/Lista de Exercicios/Exercicio 12/ex_12_adj.c
+++ Aulas/Semestre III/AED II/Grafos/Lista de Exercicios/Exercicio 12/ex_12_adj.c
@@ -18,20 +18,30 @@ typedef struct {
     int flag; // 0 = não visitado, 1 = em recursão, 2 = finalizado
 } VERTICE;
 
-bool profundidadeComCiclo(VERTICE *g, int i) {
+bool profundidadeComCiclo(VERTICE *g, int i, int ant) {
     g[i].flag = 1; // marca como "em recursão"
     NO *p = g[i].inicio;
 
     while (p) {
         if (g[p->adj].flag == 0) {
-            if (!profundidadeComCiclo(g, p->adj)) return false;
-        } else if (g[p->adj].flag == 1) {
+            if (profundidadeComCiclo(g, p->adj, i)) return true;
+        } else if (p->adj != ant) {
             // Encontrou um vértice em recursão ativa: ciclo detectado
-            return false;
+            return true;
         }
         p = p->prox;
     }
 
-    g[i].flag = 2; // marca como "visitado completamente"
-    return true;
+    return false;
+}
+
+bool temCiclo(VERTICE *g) {
+    zerarFlags(g);  // zera g[i].flag = 0 para todos os vértices
+
+    for (int i = 1; i <= V; i++) {
+        if (g[i].flag == 0) {
+            if (profundidadeComCiclo(g, i, -1)) return true;
+        }
+    }
+    return false;  // nenhum ciclo detectado
 }
