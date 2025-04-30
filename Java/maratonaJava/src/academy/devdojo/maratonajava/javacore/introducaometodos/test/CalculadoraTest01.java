88710fc09b0e0591a85ee2323ff7d0ac63d88eed
Pabl0Maciel <pablocaballero07@gmail.com>
2025-04-30 20:24:14 -0300

    Java - POO

---
+++ Java/maratonaJava/src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+        calculadora.multiplicaDoisNumeros(5, 10);
+        System.out.println(calculadora.divideDoisNumeros(10, 5));
+
+        System.out.println("Depois de criar o metodo de impressao:");
+        calculadora.imprimeDivisaoDoisNumeros(10, 5);
+
+    }
+}
